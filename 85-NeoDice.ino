b1466d72892ca0cf82fc42c39595328a0823d669
Klaus <44336145+kwolf1909@users.noreply.github.com>
2026-08-20 16:26:10 +0200

    Migration from FastLed to NeoPixel library

--- 85-NeoDice.ino
+++ 85-NeoDice.ino
@@ -1,12 +1,15 @@
-#include <FastLED.h>
-
+#define WIRE
 //#define TINYWIREM
-#define SOFTI2C
+//#define TINYI2C
+//#define SOFTWIRE
 
 #ifdef TINYWIREM
 #include <TinyWireM.h>
 #endif
-#ifdef SOFTI2C
+#ifdef TINYI2C
+#include <TinyI2CMaster.h>
+#endif
+#ifdef SOFTWIRE
 #define SDA_PORT PORTB
 #define SDA_PIN 3
 #define SCL_PORT PORTB
@@ -14,15 +17,32 @@
 #define I2C_FASTMODE 1
 #include <SoftWire.h>
 #endif
-
+#ifdef WIRE
+#include <Wire.h>
+#endif
+#include <tinyNeoPixel.h>
+//#define SEG14
 #include "AlphaDisplay.h"
 
-#define DATA_PIN            2
-#define BUTTON_PIN          0
+//#define SERIALDEBUG
+
+#if defined (__AVR_ATtiny85__)
+#define BUTTON_PIN          3
+#define DATA_PIN            4
+#endif
+#if defined (__AVR_ATmega328P__)
+#define BUTTON_PIN          2
+#define DATA_PIN            A3
+#endif
+#if defined (__AVR_ATtiny1614__)
+#define BUTTON_PIN          3
+#define DATA_PIN            PIN_PA4
+#define VIO_PIN             2
+#endif
 #define LED_TYPE            WS2812B
 #define COLOR_ORDER         GRB
 #define NUM_LEDS            24
-#define BRIGHTNESS          64
+#define BRIGHTNESS          32
 #define FRAMES_PER_SECOND   50
 #define FADE_STEPS          32
 #define PATTERN_LENGTH      12
@@ -38,25 +58,9 @@
 #define DIVIDER_FLASH7      10
 #define DIVIDER_RANDOM      20
 
-#define SERIAL
-//#define SERIALDEBUG
-
-#define HUE_RED             0
-#define HUE_ORANGE          32
-#define HUE_YELLOW          64
-#define HUE_GREEN           96
-#define HUE_AQUA            128
-#define HUE_BLUE            160
-#define HUE_PURPLE          192
-#define HUE_PINK            224
-
-#define COLOR_DICE1         HUE_GREEN
-#define COLOR_DICE2         HUE_AQUA
-#define COLOR_DICE7         HUE_YELLOW
-#define COLOR_RUNNING       HUE_PURPLE
-
 bool     waitForFirstZero, toggle;
 uint8_t  state, result1, result2, fade, pos1, pos2, sum, divider, dividerRandom, fadeCounter;
+uint16_t hue;
 uint32_t currentTime, animationTime, zeroTime;
 
 struct dicePattern {
@@ -64,60 +68,119 @@ struct dicePattern {
   uint16_t pattern;
 };
 
-enum { DICE_INIT = 1, DICE_WAIT, DICE_RUNNING, DICE_SLOWDOWN1, DICE_SLOWDOWN2, DICE_PRINT, DICE_TEST };
+enum { DICE_INIT = 1, DICE_WAIT, DICE_RUNNING, DICE_SLOWDOWN1, DICE_SLOWDOWN2, DICE_PRINT, DICE_CONFETTI, DICE_TEST };
 
 const dicePattern pattern[] = { { 5, 0b000001000000 }, { 4, 0b000010100000 }, { 3, 0b000101010000 },
-                                { 2, 0b001010101000 }, { 1, 0b010101010100 }, { 0, 0b101010101010 } };
+  { 2, 0b001010101000 }, { 1, 0b010101010100 }, { 0, 0b101010101010 }
+};
 
-CRGB leds[NUM_LEDS];
+// A 256-byte pre-calculated 8-bit sine wave lookup table (0 to 255)
+// This matches FastLED's internal sin8 lookup for high performance.
+const uint8_t PROGMEM sin8_lut[] = {
+  128, 131, 134, 137, 140, 143, 146, 149, 152, 155, 158, 162, 165, 167, 170, 173,
+  176, 179, 182, 185, 188, 190, 193, 196, 198, 201, 203, 206, 208, 211, 213, 215,
+  218, 220, 222, 224, 226, 228, 230, 232, 234, 235, 237, 238, 240, 241, 243, 244,
+  245, 246, 248, 249, 250, 250, 251, 252, 253, 253, 254, 254, 254, 255, 255, 255,
+  255, 255, 255, 255, 254, 254, 254, 253, 253, 252, 251, 250, 250, 249, 248, 246,
+  245, 244, 243, 241, 240, 238, 237, 235, 234, 232, 230, 228, 226, 224, 222, 220,
+  218, 215, 213, 211, 208, 206, 203, 201, 198, 196, 193, 190, 188, 185, 182, 179,
+  176, 173, 170, 167, 165, 162, 158, 155, 152, 149, 146, 143, 140, 137, 134, 131,
+  128, 124, 121, 118, 115, 112, 109, 106, 103, 100, 97,  93,  90,  88,  85,  82,
+  79,  76,  73,  70,  67,  65,  62,  59,  57,  54,  52,  49,  47,  44,  42,  40,
+  37,  35,  33,  31,  29,  27,  25,  23,  21,  20,  18,  17,  15,  14,  12,  11,
+  10,  9,   7,   6,   5,   5,   4,   3,   2,   2,   1,   1,   1,   0,   0,   0,
+  0,   0,   0,   0,   1,   1,   1,   2,   2,   3,   4,   5,   5,   6,   7,   9,
+  10,  11,  12,  14,  15,  17,  18,  20,  21,  23,  25,  27,  29,  31,  33,  35,
+  37,  40,  42,  44,  47,  49,  52,  54,  57,  59,  62,  65,  67,  70,  73,  76,
+  79,  82,  85,  88,  90,  93,  97,  100, 103, 106, 109, 112, 115, 118, 121, 124
+};
+
+uint8_t beatsin8(uint16_t, uint8_t, uint8_t, uint32_t, uint8_t);
+void fadeToBlack(uint8_t);
 
 AlphaDisplay alpha;
+
+tinyNeoPixel ring(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
+
+const uint32_t colorRed = ring.Color(255, 0, 0);
+const uint32_t colorOrange = ring.Color(255, 165, 0);
+const uint32_t colorYellow = ring.Color(255, 255, 0);
+const uint32_t colorGreen = ring.Color(0, 255, 0);
+const uint32_t colorCyan = ring.Color(0, 255, 255);
+const uint32_t colorBlue = ring.Color(0, 0, 255);
+const uint32_t colorMagenta = ring.Color(255, 0, 255);
+
+//---------------------------------------------------------------------------------
 
 void setup() {
   delay(100);
 
-#ifdef SERIAL
-  Serial.begin(9600);
-//Serial.println("Init...");
+#ifdef SERIALDEBUG
+  // use pins PA1(TX) and PA2(RX)
+  Serial.swap(1);
+  Serial.begin(115200);
+  Serial.println("Init...");
   delay(100);
 #endif
 
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
-  FastLED.clear();
-  FastLED.show();
-
-#ifdef TINYWIREM
+#if defined (TINYWIREM)
   TinyWireM.begin();
+#elif defined (TINYI2C)
+  TinyI2C.init();
 #else
   Wire.begin();
 #endif
 
+  // set IO-level for alphanumeric display
+#if defined(__AVR_ATtiny1614__)
+  PORTB.DIRSET = 1 << VIO_PIN;
+  PORTB.OUTSET = 1 << VIO_PIN;
+#endif
+
+  delay(100);
   alpha.init(DISPLAY_ADDRESS, DISPLAY_DIGITS, DISPLAY_BRIGHTNESS);
   alpha.print("IN IT");
 
-  uint8_t hue = 0;
+  // button input pin with pullup enabled
+#if defined (__AVR_ATtiny1614__)
+  PORTA.DIRCLR = 1 << BUTTON_PIN;
+  PORTA.PIN3CTRL = PORT_PULLUPEN_bm;
+#endif
+#if defined (__AVR_ATtiny85__)
+  DDRB &= ~(1 << BUTTON_PIN);
+  PORTB |= 1 << BUTTON_PIN;
+#endif
+
+  // NeoPixel LEDs
+  ring.begin();
+  ring.setBrightness(BRIGHTNESS);
+  ring.clear();
+  ring.show();
+
+  hue = 0;
+  uint32_t color;
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(hue, 255, 255);
-    hue += 10;
-    if (i) leds[i - 1] = 0;
-    FastLED.show();
+    color = ring.ColorHSV(hue, 255, 255);
+    ring.clear();
+    ring.setPixelColor(i, color);
+    hue += 32768 / NUM_LEDS;
+    ring.show();
     delay(ANIMATION_DELAY * 2);
   }
-  FastLED.clear();
-  FastLED.show();
+  ring.clear();
+  ring.show();
 
   alpha.clear();
-  
+
+  hue = 0;
   animationTime = millis();
   state = DICE_INIT;
 }
 
+//---------------------------------------------------------------------------------
+
 void loop()
-{  
+{
   currentTime = millis();
   if (currentTime - animationTime > ANIMATION_DELAY) {
     animationTime = currentTime;
@@ -128,32 +191,38 @@ void loop()
         result1 = 0;
         result2 = 0;
         dividerRandom = 0;
-        state = DICE_WAIT;
+        state = DICE_CONFETTI;
+#ifdef SERIALDEBUG
+        Serial.println("State: Confetti");
+#endif
         break;
 
       case DICE_WAIT:
         if (readButton(BUTTON_PIN)) {
+#ifdef SERIALDEBUG
+          Serial.println("State: Running");
+#endif
           state = DICE_RUNNING;
-          zeroTime = millis() + ANIMATION_DELAY;
+          zeroTime = millis();
           waitForFirstZero = true;
           divider = 0;
         }
-#ifdef SERIAL
+#ifdef SERIALDEBUG
         if (Serial.available() > 0) {
           uint8_t c = Serial.read();
           if (c == 'x') {
+            Serial.println("State: Running");
             state = DICE_RUNNING;
-            zeroTime = millis() + ANIMATION_DELAY;
+            zeroTime = millis();
             waitForFirstZero = true;
             divider = 0;
           }
           if (c == 't') state = DICE_TEST;
         }
 #endif
-
         // periodically call random function to get better randomness
-        if (dividerRandom++ == 0) random(2,13);
-        if (dividerRandom >= DIVIDER_RANDOM) dividerRandom = 0;  
+        if (dividerRandom++ == 0) random(2, 13);
+        if (dividerRandom >= DIVIDER_RANDOM) dividerRandom = 0;
 
         // animation
         if (!result1 || !result2) break;
@@ -169,40 +238,40 @@ void loop()
           case 8:
             if (fade) {
               addGlitter(30);
-              fadeToBlackBy(leds, NUM_LEDS, 32);
-              printDice(result1, PATTERN_LENGTH, 0, COLOR_DICE1);
-              printDice(result2, PATTERN_LENGTH, PATTERN_LENGTH, COLOR_DICE2);
+              fadeToBlack(16);
+              printDice(result1, PATTERN_LENGTH, 0, colorCyan);
+              printDice(result2, PATTERN_LENGTH, PATTERN_LENGTH, colorGreen);
               fade--;
             }
             else {
               fade = FADE_STEPS;
             }
-            FastLED.show();
-            
+            ring.show();
+
             if (divider++ == 0) {
               showVal(sum, toggle);
               toggle = !toggle;
             }
-            if (divider >= DIVIDER_FLASH68) divider = 0;  
+            if (divider >= DIVIDER_FLASH68) divider = 0;
             break;
 
           case 7:
             if (fade) {
-              fadeToBlackBy(leds, NUM_LEDS, 16);
+              fadeToBlack(16);
               fade--;
             }
             else {
-              printDice(result1, PATTERN_LENGTH, 0, COLOR_DICE7);
-              printDice(result2, PATTERN_LENGTH, PATTERN_LENGTH, COLOR_DICE7);
+              printDice(result1, PATTERN_LENGTH, 0, colorYellow);
+              printDice(result2, PATTERN_LENGTH, PATTERN_LENGTH, colorYellow);
               fade = FADE_STEPS;
             }
-            FastLED.show();
+            ring.show();
 
             if (divider++ == 0) {
               showVal(sum, toggle);
               toggle = !toggle;
             }
-            if (divider >= DIVIDER_FLASH7) divider = 0;  
+            if (divider >= DIVIDER_FLASH7) divider = 0;
             break;
 
           case 9:
@@ -217,68 +286,84 @@ void loop()
         break;
 
       case DICE_RUNNING:
-        fadeToBlackBy(leds, NUM_LEDS, 32);
+        fadeToBlack(64);
         pos1 = beatsin8(20, 0, NUM_LEDS - 1, zeroTime, 192);
-        leds[pos1] = CHSV(COLOR_RUNNING, 255, 255);
-        if (pos1 + PATTERN_LENGTH < NUM_LEDS) pos2 = pos1 + PATTERN_LENGTH;
-        else pos2 = pos1 + PATTERN_LENGTH - NUM_LEDS;
-        leds[pos2] = CHSV(COLOR_RUNNING, 255, 255);
-        FastLED.show();
-#ifdef SERIALDEBUG
-        Serial.printf("%u, %u\n\r", pos1, pos2);
-#endif
+        pos2 = (pos1 + PATTERN_LENGTH) % NUM_LEDS;
+        ring.setPixelColor(pos1, colorMagenta);
+        ring.setPixelColor(pos2, colorMagenta);
+        ring.show();
+
+        if (divider++ == 0) showVal(random(2, 13), true);
+        if (divider >= DIVIDER_RUNNING) divider = 0;
+        if (pos1) waitForFirstZero = false;
+
         if (pos1 == 0 && waitForFirstZero == false) {
+#ifdef SERIALDEBUG
+          Serial.println("State: Slowdown1");
+#endif
           state = DICE_SLOWDOWN1;
-          zeroTime = millis() + ANIMATION_DELAY;
+          zeroTime = millis();
           result1 = random(1, 7);
           result2 = random(1, 7);
         }
-        if (divider++ == 0) showVal(random(2, 13), true);
-        if (divider >= DIVIDER_RUNNING) divider = 0;
-
-        if (pos1) waitForFirstZero = false;
         break;
 
       case DICE_SLOWDOWN1:
-        fadeToBlackBy(leds, NUM_LEDS, 32);
+        fadeToBlack(4);
         pos1 = beatsin8(20, 0, PATTERN_LENGTH, zeroTime, 192);
-        leds[pos1] = CHSV(COLOR_DICE1, 255, 255);
-        if (pos1 + PATTERN_LENGTH < NUM_LEDS) pos2 = pos1 + PATTERN_LENGTH;
-        else pos2 = pos1 + PATTERN_LENGTH - NUM_LEDS;
-        leds[pos2] = CHSV(COLOR_DICE2, 255, 255);
+        pos2 = (pos1 + PATTERN_LENGTH) % NUM_LEDS;
+        ring.setPixelColor(pos1, colorCyan);
+        ring.setPixelColor(pos2, colorGreen);
 
-        printDice(result1, pos1, 0, COLOR_DICE1);
-        printDice(result2, pos1, PATTERN_LENGTH, COLOR_DICE2);
+        printDice(result1, pos1, 0, colorCyan);
+        printDice(result2, pos1, PATTERN_LENGTH, colorGreen);
 
-        FastLED.show();
-#ifdef SERIALDEBUG
-        Serial.printf("%u, %u\n\r", pos1, pos2);
-#endif
-        if (divider++ == 0) showVal(random(2, 13), true);
-        if (divider >= DIVIDER_SLOWDOWN1) divider = 0;
+        ring.show();
+
+        if (divider == 0) showVal(random(2, 13), true);
+        if (divider++ >= DIVIDER_SLOWDOWN1) divider = 0;
 
         if (pos1 == PATTERN_LENGTH - 1) {
+#ifdef SERIALDEBUG
+          Serial.println("State: Slowdown2");
+#endif
           state = DICE_SLOWDOWN2;
           fadeCounter = 60;
         }
         break;
 
       case DICE_SLOWDOWN2:
-        fadeToBlackBy(leds, NUM_LEDS, 16);
-        printDice(result1, PATTERN_LENGTH, 0, COLOR_DICE1);
-        printDice(result2, PATTERN_LENGTH, PATTERN_LENGTH, COLOR_DICE2);
-        FastLED.show();
+        fadeToBlack(4);
+        printDice(result1, PATTERN_LENGTH, 0, colorCyan);
+        printDice(result2, PATTERN_LENGTH, PATTERN_LENGTH, colorGreen);
+        ring.show();
 
-        if (divider++ == 0) showVal(random(2, 13), true);
-        if (divider >= DIVIDER_SLOWDOWN2) divider = 0;
+        if (divider == 0) showVal(random(2, 13), true);
+        if (divider++ >= DIVIDER_SLOWDOWN2) divider = 0;
 
-        if (fadeCounter-- == 0) {
+        if (--fadeCounter == 0) {
           sum = result1 + result2;
           divider = 0;
           toggle = true;
           showVal(sum, true);
+#ifdef SERIALDEBUG
+          Serial.println("State: Wait");
+#endif
           state = DICE_WAIT;
         }
+        break;
+
+      case DICE_CONFETTI:
+        confetti(hue++);
+
+        if (readButton(BUTTON_PIN)) {
+#ifdef SERIALDEBUG
+          Serial.println("State: Wait");
+#endif
+          state = DICE_WAIT;
+          ring.clear();
+        }
+        ring.show();
         break;
 
       case DICE_TEST:
@@ -292,17 +377,16 @@ void loop()
           for (uint16_t i = 0; i < NUM_TEST; i++) {
             result1 = random(1, 7);
             result2 = random(1, 7);
-            if(result1 + result2 == x) numResult++;
+            if (result1 + result2 == x) numResult++;
           }
           numResult /= 10;
-          
+
           if (x >= 10) disp[0] = '0' + x / 10;
           else disp[0] = ' ';
           disp[1] = '0' + x % 10;
-          disp[2] = ' ';
-          if(numResult >= 10) disp[3] = '0' + numResult / 10;
-          else disp[3] = ' ';
-          disp[4] = '0' + numResult % 10;
+          if (numResult >= 10) disp[2] = '0' + numResult / 10;
+          else disp[2] = ' ';
+          disp[3] = '0' + numResult % 10;
           alpha.print(disp);
           delay(1000);
         }
@@ -312,7 +396,9 @@ void loop()
   }
 }
 
-void printDice(uint8_t val, uint8_t maxPos, uint8_t offset, uint8_t color) {
+//---------------------------------------------------------------------------------
+
+void printDice(uint8_t val, uint8_t maxPos, uint8_t offset, uint32_t color) {
   uint16_t p;
 
   if (offset + PATTERN_LENGTH > NUM_LEDS) return;
@@ -321,16 +407,7 @@ void printDice(uint8_t val, uint8_t maxPos, uint8_t offset, uint8_t color) {
 
   // write dice pattern
   for (uint8_t pos = 0; pos < maxPos; pos++) {
-
-    if ((p >> pos) & 0x0001)
-      leds[offset + pos] = CHSV( color, 255, 255);
-    //  else
-    //    leds[offset + pos] = CHSV( 0, 0, 0);
-  }
-}
-void addGlitter(fract8 chanceOfGlitter) {
-  if (random8() < chanceOfGlitter) {
-    leds[random16(NUM_LEDS)] += CRGB::White;
+    if ((p >> pos) & 0x0001) ring.setPixelColor(offset + pos, color);
   }
 }
 
@@ -352,12 +429,116 @@ void showVal(uint8_t val, bool on) {
 }
 
 bool readButton(uint8_t pin) {
-  bool val;
-  
-  // read pin by polling
-  DDRB &= ~(1 << pin);
-  delay(1);
-  val = PINB & (1 << pin);
-  DDRB |= 1 << pin;
-  return !val;
+
+#if defined(__AVR_ATtiny1614__)
+  return PORTA.IN & (1 << BUTTON_PIN) ? false : true;
+#endif
+#if defined (__AVR_ATtiny85__)
+  return PINB & (1 << BUTTON_PIN) ? false : true;
+#endif
+  return false;
+}
+
+uint8_t beatsin8(uint16_t bpm, uint8_t low, uint8_t high, uint32_t time_offset, uint8_t phase_offset) {
+  // 60.000 ms pro Minute / BPM = Dauer eines vollen Taktes in ms
+  uint32_t ms_per_beat = 60000L / bpm;
+
+  // Aktuelle Zeit plus den gewünschten Zeit-Versatz berechnen
+  uint32_t adjusted_time = millis() - time_offset;
+
+  // Position innerhalb des aktuellen Taktes ermitteln (Modulo-Überlauf)
+  uint32_t pos = adjusted_time % ms_per_beat;
+
+  // Zeit-Position sauber auf den Indexbereich 0-255 skalieren
+  uint8_t base_index = (pos * 255) / ms_per_beat;
+
+  // Phasenversatz hinzufügen (8-Bit Integer läuft automatisch bei 255 sauber auf 0 über)
+  uint8_t final_index = base_index + phase_offset;
+
+  // Den echten Sinuswert (0 bis 255) aus dem Flash-Speicher auslesen
+  uint8_t wave = pgm_read_byte(&(sin8_lut[final_index]));
+
+  // Welle auf das gewünschte Ziel-Fenster [low, high] skalieren
+  uint16_t range = high - low;
+
+#ifdef SERIALDEBUG2
+  char buf[80];
+  sprintf(buf, "beatsin8: pos: %lu, base_index: %u, final_index: %u, wave: %u", pos, base_index, final_index, wave);
+  Serial.println(buf);
+#endif
+
+  return low + ((wave * range) / 255);
+}
+
+void fadeToBlack(uint8_t fadeValue) {
+  for (uint8_t i = 0; i < ring.numPixels(); i++) {
+    uint32_t c = ring.getPixelColor(i);
+    uint16_t r = (c >> 16) & 0xFF;
+    uint16_t g = (c >> 8) & 0xFF;
+    uint16_t b = c & 0xFF;
+
+    // Scale down colors (255 - fadeValue) / 256
+    // If fadeValue is 64, it multiplies by 191/256 (~0.75)
+    r = (r * (255 - fadeValue)) >> 8;
+    g = (g * (255 - fadeValue)) >> 8;
+    b = (b * (255 - fadeValue)) >> 8;
+
+#ifdef SERIALDEBUG3
+  char buf[80];
+  sprintf(buf, "fadeToBlack: R: %u, G: %u, B: %u", r, g, b);
+  Serial.println(buf);
+#endif
+
+    ring.setPixelColor(i, ring.Color(r, g, b));
+  }
+}
+
+void addGlitter(uint8_t chanceOfGlitter) {
+  if (random(256) < chanceOfGlitter) {
+    int pos = random(ring.numPixels());
+    uint32_t current = ring.getPixelColor(pos);
+
+    // Extract RGB components using bitwise math
+    uint8_t r = (current >> 16) & 0xFF;
+    uint8_t g = (current >> 8) & 0xFF;
+    uint8_t b = current & 0xFF;
+
+    // Add white with saturation cap at 255
+    r = (r + 255 > 255) ? 255 : r + 255; // effectively sets to white max
+    // Or to actually mix/add: r = qadd8(r, 255) if using FastLED lib8tion,
+    // but with pure standard Arduino integer math:
+    r = (uint16_t)r + 255 > 255 ? 255 : r + 255; // simplified below:
+
+    // Cleaner additive white assignment:
+    ring.setPixelColor(pos, ring.Color(min(255, r + 255), min(255, g + 255), min(255, b + 255)));
+  }
+}
+
+void confetti(uint16_t baseHue) {
+  // Fade all existing pixels down by multiplying RGB by 245/256 (~4% fade per frame)
+  for (uint16_t i = 0; i < ring.numPixels(); i++) {
+    uint32_t c = ring.getPixelColor(i);
+    if (c != 0) {
+      uint8_t r = (c >> 16) & 0xFF;
+      uint8_t g = (c >> 8) & 0xFF;
+      uint8_t b = c & 0xFF;
+
+      // Integer math multiplication & shift (fade to black)
+      r = (r * 245) >> 8;
+      g = (g * 245) >> 8;
+      b = (b * 245) >> 8;
+
+      ring.setPixelColor(i, r, g, b);
+    }
+  }
+
+  // Pick a random pixel and add a bright speckle of color
+  uint16_t pos = random(ring.numPixels());
+
+  // Combine base hue with a random offset (0-63 range out of 65535 HSV hue space)
+  uint16_t randomHue = (baseHue + random(64)) * 1024;
+  uint32_t color = ring.ColorHSV(randomHue, 200, 255);
+
+  // Blend by adding or simply setting the new speckle
+  ring.setPixelColor(pos, color);
 }
